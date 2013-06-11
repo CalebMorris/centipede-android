f88136cf207599e4d275c11160c3b6acd3d77bce
Caleb Morris <caleb.morris.g@gmail.com>
2013-06-10 18:59:08 -0700

    basic settings activity

---
+++ Centipede/src/main/java/com/blogspot/turtldev/centipede/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.blogspot.turtldev.centipede;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cdbitesky on 6/10/13.
+ */
+public class SettingsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
