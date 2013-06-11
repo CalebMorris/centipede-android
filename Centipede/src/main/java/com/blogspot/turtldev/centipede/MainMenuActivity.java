8209621e57e9ea192b9c017dbb50db145e9dc6a7
Caleb Morris <caleb.morris.g@gmail.com>
2013-06-10 19:01:10 -0700

    pass and recieve settings

--- Centipede/src/main/java/com/blogspot/turtldev/centipede/MainMenuActivity.java
+++ Centipede/src/main/java/com/blogspot/turtldev/centipede/MainMenuActivity.java
@@ -19,5 +19,34 @@ public class MainMenuActivity extends Activity {
         startActivity(intent);
     }
 
+    public void onSettingsClick( View view ) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case (2) : {
+                //if (resultCode == Activity.RESULT_OK) {
+                    // TODO Extract the data returned from the child Activity.
+                    if( data != null ) {
+                        int message = data.getIntExtra("SPEED", 16);
+                        speed = message;
+                        Log.v("result","size:"+message);
+                        message = data.getIntExtra("SIZE", 100);
+                        size = message;
+                        Log.v("result","speed:"+message);
+                    }
+                    else {
+                        Log.v("result","Settings haven't been changed");
+                    }
+                //}
+                break;
+            }
+        }
+    }
+
     //TODO Settings Activity && Settings button intent
 }
--- Centipede/src/main/java/com/blogspot/turtldev/centipede/SettingsActivity.java
+++ Centipede/src/main/java/com/blogspot/turtldev/centipede/SettingsActivity.java
@@ -1,13 +1,45 @@
 package com.blogspot.turtldev.centipede;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by cdbitesky on 6/10/13.
  */
 public class SettingsActivity extends Activity {
+    EditText speed;
+    EditText size;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        speed = (EditText)findViewById(R.id.editText_speed);
+        size = (EditText)findViewById(R.id.editText_size);
+
+        size.setText("16");
+        speed.setText("100");
+    }
+
+    public void onAcceptClick( View view ) {
+        // Gather new information from edit_texts, if within bounds return the data
+        int message_speed = Integer.parseInt(speed.getText().toString());
+        int message_size = Integer.parseInt(size.getText().toString());
+        Intent intentMessage = new Intent();
+
+        intentMessage.putExtra("SIZE",message_size);
+        intentMessage.putExtra("SPEED",message_speed);
+        setResult(2, intentMessage);
+
+        finish();
+    }
+
+    public void onCancelClick( View view ) {
+        // Return nothing and finish the activity
+        finish();
     }
 }
